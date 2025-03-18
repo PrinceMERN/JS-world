692d2b2ef590c80755d7c524f9b94de4a42fb5eb
prince <prince9354200453@gmail.com>
2025-03-18 17:41:41 +0000

    container

---
+++ 01_basics/test.js
@@ -0,0 +1,19 @@
+const accountId = 14456;
+let accountEmail ="hi@gmail.com"
+var accountPassword = "1234"
+
+accountcity="jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "hc@gmail.com"
+accountPassword = "122112"
+accountcity = "goa"
+console.log(accountId)
+
+/*
+Prefer not to use var because of issue in black scope and funcional scope 
+/*
+
+console.table([accountId, accountEmail,accountPassword,accountcity]);
