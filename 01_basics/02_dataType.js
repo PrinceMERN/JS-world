46056712d6b3ad597de2ad5ba08ed8edf31ae45f
prince <prince9354200453@gmail.com>
2025-03-20 06:59:55 +0000

    container config

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version //
+
+//alert(3 + 3) //we are using node.js not browser //
+
+console.log( 3 
+    + 3)  // code readabilty should be high//
+
+    console.log("prince")
+
+    let name = "prince"
+    let age = 19;
+    let isLooggedIn = false;
+
+    // number => 2 to 53
+    // bigint 
+    // string => ""
+    // boolean => true/false
+    // null => standalone value
+    // undefined =>
+    // symbol => unique
+    
+    //object
+
+    console.log(typeof null); // object
+    console.log(typeof undefined); // undefined
