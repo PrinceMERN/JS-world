721b46f056e5d283bff8c3f1caba38a87702b280
prince <prince9354200453@gmail.com>
2025-03-20 11:02:03 +0000

    container config

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,12 +1,12 @@
 let score = "33prince"
 
-console.log(typeof score);
-console.log(typeof (score))
+// console.log(typeof score);
+// console.log(typeof (score))
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 // "33" => 33
 // "33abx" => NaN
@@ -16,7 +16,7 @@ console.log(valueInNumber)
 let isLoggedIn = "1"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true
 // 0 => false
@@ -25,5 +25,41 @@ console.log(booleanIsLoggedIn)
 let someNumber = 33
 
 let turnSomeNumber = String(someNumber)
-console.log(turnSomeNumber)
-console.log(typeof turnSomeNumber);
+// console.log(turnSomeNumber)
+// console.log(typeof turnSomeNumber);
+
+let value = 5
+let netValue = -value
+console.log(netValue)
+
+// console.log(2+2) // Plus 4
+// console.log(2-2) // Minus 0
+// console.log(2*2) // multiple 4
+// console.log(2**2) // Powar 
+// console.log(2/3) // divide 1.5
+// console.log(2%3) // reminder 1
+
+let str1 = "Hello"
+let str2 = " Prince"
+
+let str3 = str1 + str2
+console.log(str3)
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2)
+console.log( 1 + 2 + "2" )
+console.log( (3 + 2) * 4 % 5 )
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+console.log(num1)
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
+
+// tc39
