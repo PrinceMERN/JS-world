10c0cb3467ce8a28d63e6009f3b103730c16cec1
prince <prince9354200453@gmail.com>
2025-03-20 07:32:19 +0000

    container config

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,29 @@
+let score = "33prince"
+
+console.log(typeof score);
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abx" => NaN
+// true => 1
+// false => 0
+
+let isLoggedIn = "1"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true
+// 0 => false
+// "" => false
+
+let someNumber = 33
+
+let turnSomeNumber = String(someNumber)
+console.log(turnSomeNumber)
+console.log(typeof turnSomeNumber);
