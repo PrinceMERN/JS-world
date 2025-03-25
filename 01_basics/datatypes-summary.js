20159353055127f7e16ef4d758c2837385b35ca0
prince <prince9354200453@gmail.com>
2025-03-25 12:15:46 +0000

    container config

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,40 @@
+// primitive
+
+// 7 types : String, Number, Boolean, Null, Undefined, Symbal, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+const bigNumber = 4544654464464454n
+
+console.log(typeof bigNumber)
+
+// Reference (Non primitive) 
+
+// Array, Objects, Function
+
+const heros = ["Batman", "Ironman", "Speiderman", "Antman"];
+
+let myObj = 
+{
+    name : "prince",
+    age : 14,
+
+}
+
+const myFunction = function(){
+    console.log("hello world")
+}
+
+console.log(typeof heros)
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
