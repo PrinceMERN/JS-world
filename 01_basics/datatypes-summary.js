6bda2a5d00e480f138e6ea9917e14c322938ddd2
prince <prince9354200453@gmail.com>
2025-03-25 14:40:51 +0000

    container config

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -12,11 +12,11 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId)
+// console.log(id === anotherId)
 
 const bigNumber = 4544654464464454n
 
-console.log(typeof bigNumber)
+// console.log(typeof bigNumber)
 
 // Reference (Non primitive) 
 
@@ -35,6 +35,33 @@ const myFunction = function(){
     console.log("hello world")
 }
 
-console.log(typeof heros)
+// console.log(typeof heros)
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack (Primitive), Heap (Non-primitive)
+
+
+
+let myYoutubeName = "Prince"
+
+let anotherName  = myYoutubeName 
+anotherName = "Govind"
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+let userOne = {
+    email: "prince@gmail.com",
+    upi : "user@sbi"
+}
+
+let userTwo = userOne
+
+userTwo.email = "user@gmail.com"
+
+console.log(userOne)
+console.log(userTwo)
+
