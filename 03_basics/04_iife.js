adf47e179bce37d4b4d851d5afc8ba1b37690482
prince <prince9354200453@gmail.com>
2025-05-05 12:26:08 +0000

    container config

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,34 @@
+// Immediatly Invoked function expression //
+
+// function chai() {
+//     console.log("DB connected")
+// }
+// chai()
+
+// (function chai() {
+//     console.log("DB connected")
+// })();
+
+// (function code() {
+//     console.log("DB connected ")
+// })();
+
+// ((name) => {
+//     console.log(`DB connected to ${name}`)
+// })("prince")
+
+// function one() {
+//     console.log("one")   
+//     two() 
+// }
+// function two(){
+//     console.log("Two")
+//     three()
+// }
+// function three(){
+//     console.log("three")
+// }
+
+// one()
+// two()
+// three()
